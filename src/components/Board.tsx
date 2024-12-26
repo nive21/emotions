73f36df85473906c8f99cc313642d840a4edbfd9
Nivedhitha <nive.mathan21@gmail.com>
2024-12-26 02:13:29 -0500

    add sticky notes

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import styles from "../styles/Board.module.scss";
 
+const COLORS = [
+  { name: "Reds", hue: 0 },
+  { name: "Greens", hue: 90 },
+  { name: "Blues", hue: 180 },
+  { name: "Voilets", hue: 270 },
+];
+const COL = 5;
+const ROW = 10;
+
 // The board of emotions
 function Board() {
   const [tab, setTab] = useState(0);
@@ -56,7 +65,67 @@ function Navbar({
 
 // The supplies tab of the board
 function Supplies() {
-  return <>supplies</>;
+  const [tab, setTab] = useState(0);
+  return (
+    <>
+      <ColorsTab {...{ tab, setTab }} />
+      <AllNotes {...{ tab }} />
+    </>
+  );
+}
+
+function ColorsTab({
+  tab,
+  setTab,
+}: {
+  tab: number;
+  setTab: (tab: number) => void;
+}) {
+  return (
+    <div className={styles.colors__tab}>
+      {COLORS.map((color, i) => (
+        <button
+          className={`${styles.colors__title} ${
+            tab === i ? styles.active : ""
+          }`}
+          onClick={() => setTab(i)}
+        >
+          {color.name}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+function AllNotes({ tab }: { tab: number }) {
+  return (
+    <div className={styles.supplies__container}>
+      {Array.from({ length: COL }, (_, i) => (
+        <NotesRow hue={i * 10 + COLORS[tab].hue} />
+      ))}
+    </div>
+  );
+}
+
+function NotesRow({ hue }: { hue: number }) {
+  return (
+    <div className={styles.notes__row}>
+      {Array.from({ length: ROW }, (_, i) => (
+        <EmptyNote lightness={96 - i} hue={hue} />
+      ))}
+    </div>
+  );
+}
+
+function EmptyNote({ lightness, hue }: { lightness: number; hue: number }) {
+  return (
+    <div
+      className={styles.note}
+      style={{
+        backgroundColor: `hsl(${hue}, 74%, ${lightness}%)`,
+      }}
+    ></div>
+  );
 }
 
 // The calendar tab of the board
