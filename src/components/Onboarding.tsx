9b8e513d472273f096c4f3ea4529b74c9b1f28e6
Nivedhitha <nive.mathan21@gmail.com>
2024-12-25 22:15:00 -0500

    first commit

---
+++ src/components/Onboarding.tsx
@@ -0,0 +1,16 @@
+function Onboarding() {
+  return (
+    <div>
+      <p>
+        Identifying and distinguishing <span>emotions</span> clearly can improve
+        well-being.
+      </p>
+      <button>Okay</button>
+      <p>
+        Made with ♡ by <a href="https://www.linkedin.com/in/nivemathan/">NK</a>
+      </p>
+    </div>
+  );
+}
+
+export default Onboarding;
